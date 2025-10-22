92a03eb2c97b5d6f2426819d5e004c1e4b7e7d3d
poiss <poiss@users.noreply.github.com>
2025-10-22 02:49:34 -0400

    fixing hero

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="w-full h-screen flex items-center justify-center">
         <HeroBackground />
         <div className="w-4/5 h-auto flex flex-col gap-6 md:z-100">
-          <h1 className="lg:text-[140px] lg:leading-[128px]  md:text-[100px] md:leading-[90px] text-[64px] leading-[70px] font-sans-heavy text-[#4D4D4D] pb-4">Full Stack Developer
+          <h1 className="lg:text-[100px] lg:leading-[110px]  md:text-[100px] md:leading-[90px] text-[64px] leading-[70px] font-sans-heavy text-[#4D4D4D] pb-4">Full Stack Developer
             With a Healthy Obsession
             Over Details. </h1>
           <a href="#projects" className="w-60 text-white rounded-md text-xl border-t-4 border-l-transparent border-r-transparent border-t-white/90 border-b-4 border-[#A65962]/80 h-auto flex items-center px-4 pt-4 pb-3.5 justify-center bg-linear-to-t from-accent/90 via-60% to-accent/30"> 
